30a705eb24751757e51bab660e9baf4d66d58ab8
Jacob Cao <firaflamingo@gmail.com>
2018-09-28 11:33:37 -0700

    Applied Covalent Layout, Router linked to home. --Everything Stock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatListModule, MatIconModule, MatCardModule, MatMenuModule, MatInputModule, MatButtonToggleModule,
+  MatProgressSpinnerModule, MatSelectModule, MatSlideToggleModule, MatDialogModule, MatSnackBarModule, MatToolbarModule,
+  MatTabsModule, MatSidenavModule, MatTooltipModule, MatRippleModule, MatRadioModule, MatGridListModule,
+  MatDatepickerModule, MatNativeDateModule, MatSliderModule, MatAutocompleteModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CovalentLayoutModule } from '@covalent/core/layout';
-import { CovalentStepsModule  } from '@covalent/core/steps';
 import { CovalentHttpModule } from '@covalent/http';
 import { CovalentHighlightModule } from '@covalent/highlight';
 import { CovalentMarkdownModule } from '@covalent/markdown';
 import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+
+import { CovalentCommonModule, CovalentLayoutModule, CovalentMediaModule, CovalentExpansionPanelModule,
+  CovalentStepsModule, CovalentLoadingModule, CovalentDialogsModule, CovalentSearchModule, CovalentPagingModule,
+  CovalentNotificationsModule, CovalentMenuModule, CovalentDataTableModule, CovalentMessageModule } from '@covalent/core';
+
 import { ConsumerComponent } from './consumer/consumer.component';
 import { AnalystComponent } from './analyst/analyst.component';
 import { HomeComponent } from './home/home.component';
@@ -24,12 +32,54 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+
+    //
+    //
+    //Covalent Modules
     CovalentHttpModule.forRoot(),
-    CovalentLayoutModule,
     CovalentStepsModule,
     CovalentHighlightModule,
     CovalentMarkdownModule,
     CovalentDynamicFormsModule,
+    CovalentCommonModule,
+    CovalentLayoutModule,
+    CovalentMediaModule,
+    CovalentExpansionPanelModule,
+    CovalentDialogsModule,
+    CovalentLoadingModule,
+    CovalentSearchModule,
+    CovalentPagingModule,
+    CovalentNotificationsModule,
+    CovalentMenuModule,
+    CovalentDataTableModule,
+    CovalentMessageModule,
+    //
+    //
+    // Material Modules
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatMenuModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatRippleModule,
+    MatRadioModule,
+    MatGridListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSliderModule,
+    MatAutocompleteModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  ChangeDetectionStrategy, ChangeDetectorRef, OnInit } from '@angular/core';
+import { TdMediaService } from '@covalent/core/media';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [ TdMediaService ]
 })
 export class HomeComponent implements OnInit {
+  title = 'app';
 
-  constructor() { }
+  routes: Object[] = [{
+    icon: 'home',
+    route: '.',
+    title: 'Home',
+  }, {
+    icon: 'library_books',
+    route: '.',
+    title: 'Documentation',
+  }, {
+    icon: 'color_lens',
+    route: '.',
+    title: 'Style Guide',
+  }, {
+    icon: 'view_quilt',
+    route: '.',
+    title: 'Layouts',
+  }, {
+    icon: 'picture_in_picture',
+    route: '.',
+    title: 'Components & Addons',
+  },
+];
+usermenu: Object[] = [{
+    icon: 'swap_horiz',
+    route: '.',
+    title: 'Switch account',
+  }, {
+    icon: 'tune',
+    route: '.',
+    title: 'Account settings',
+  }, {
+    icon: 'exit_to_app',
+    route: '.',
+    title: 'Sign out',
+  },
+];
+navmenu: Object[] = [{
+    icon: 'looks_one',
+    route: '.',
+    title: 'First item',
+    description: 'Item description',
+  }, {
+    icon: 'looks_two',
+    route: '.',
+    title: 'Second item',
+    description: 'Item description',
+  }, {
+    icon: 'looks_3',
+    route: '.',
+    title: 'Third item',
+    description: 'Item description',
+  }, {
+    icon: 'looks_4',
+    route: '.',
+    title: 'Fourth item',
+    description: 'Item description',
+  }, {
+    icon: 'looks_5',
+    route: '.',
+    title: 'Fifth item',
+    description: 'Item description',
+  },
+];
+
+  constructor(public media: TdMediaService) { }
 
   ngOnInit() {
+      console.log("Initialized Home Page");
+
+
   }
 
 }
